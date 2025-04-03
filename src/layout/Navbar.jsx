70df064d2291cb0c9aaeac826183620abee3867e
Tyler Quaill <tquaill@email.uscb.edu>
2025-04-02 22:13:31 -0400

    Created SignUp.jsx with basic functionally until backend implementation

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        Echodeck
-                    </a>
-                    <button className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Echodeck</Link>
+                <button className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    <div className="ms-auto">
-                        <button className="btn btn-outline-light me-2" type="button">Sign Up</button>
-                        <button className="btn btn-outline-light me-2" type="button">Login</button>
-                    </div>
+                <div className="ms-auto">
+                    <Link className="btn btn-outline-light me-2" to="/signup">Sign Up</Link>
+                    <button className="btn btn-outline-light me-2" type="button">Login</button>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }
