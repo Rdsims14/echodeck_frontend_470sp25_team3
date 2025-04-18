c862649cb37a9d79b9589e880c02bedfa9067d5e
rdsims <rdsims@email.uscb.edu>
2025-04-18 12:22:07 -0400

    I had to comment out UploadSounds.jsx because it isn't working.

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -33,13 +33,13 @@ export default function Navbar() {
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto">
-                            {isAuthenticated && (
+                            {/*isAuthenticated && (
                                 <li className="nav-item">
                                     <Link className="btn btn-outline-light" to="/uploadsounds">
                                         Upload Sound
                                     </Link>
                                 </li>
-                            )}
+                            )} */}
                         </ul>
 
                         <div className="ms-auto">
